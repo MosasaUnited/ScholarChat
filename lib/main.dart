418e2c15334710ba3a3ad929e1dae1b71ebb3e4e
MosasaUnited <mostafa447@hotmail.com>
2023-02-23 00:16:33 +0200

    BlocListener

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubit/login_cubit.dart';
 import 'package:scholar_chat/pages/login_page.dart';
 import 'package:scholar_chat/pages/resgister_page.dart';
 import 'firebase_options.dart';
 
-void main() async  {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -18,14 +20,17 @@ class ScholarChat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        LoginPage.id: (context) => LoginPage(),
-        RegisterPage.id: (context) => RegisterPage(),
-        ChatPage.id : (context) => ChatPage()
-      },
-      initialRoute: LoginPage.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          LoginPage.id: (context) => LoginPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+          ChatPage.id: (context) => ChatPage()
+        },
+        initialRoute: LoginPage.id,
+      ),
     );
   }
 }
