ef173469e34e98e2c6b38546f687e0466dcee469
MosasaUnited <mostafa447@hotmail.com>
2023-03-06 00:03:24 +0200

    ChatCubit, BlocConsumer, Triggering the Cubit, Finishing App

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubit/chat_cubit.dart';
 import 'package:scholar_chat/pages/cubit/login_cubit.dart';
 import 'package:scholar_chat/pages/cubit/register_cubit.dart';
 import 'package:scholar_chat/pages/login_page.dart';
@@ -25,6 +26,7 @@ class ScholarChat extends StatelessWidget {
       providers: [
         BlocProvider(create: (context) => LoginCubit()),
         BlocProvider(create: (context) => RegisterCubit()),
+        BlocProvider(create: (context) => ChatCubit()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
